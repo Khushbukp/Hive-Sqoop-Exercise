5fa3540cba6761e445463a9989d56531587b224a
Khushbukp <cloudera@quickstart.cloudera>
2019-03-12 09:11:13 -0700

    updated execution script

--- execution_script1.sh
+++ execution_script1.sh
@@ -1,8 +1,9 @@
+
 python /home/cloudera/practical_exercise_data_generator.py --load_data
 
 python /home/cloudera/practical_exercise_data_generator.py --create_csv
 
- ##steps:  some more changes:
+ 
 ## executing sqoop job
 
 sqoop job \
@@ -25,8 +26,8 @@ sqoop import \
 ## To ingest CSV files from the local file system into HDFS
 
 mv  *.csv   /home/cloudera/TEMP/user_upload_dump.$(date +%s).csv
-hadoop fs -put   /home/cloudera/TEMP/*.csv   /user/cloudera/exercise/    
-hadoop fs -ls  /user/cloudera/exercise/ 
+hadoop fs -put   /home/cloudera/TEMP/*.csv   /user/cloudera/workshop/exercise1    
+hadoop fs -ls  /user/cloudera/workshop/exercise1/ 
 mv /home/cloudera/TEMP/*.csv   /home/cloudera/backup/
 ls /home/cloudera/backup/
 
@@ -45,18 +46,29 @@ hive -e "select count(*) from practical_exercise_1.user_upload_dump;"
 ## generating user_report table  
 hive -e "truncate table practical_exercise_1.user_report;" 
 
-hive -e "insert into user_report Select u.id,c.TOTAL_UPDATE, c.TOTAL_INSERT,  c.TOTAL_DELETE, b.Type as Last_ACTIVITY_Type, c.IS_ACTIVE,  d.total_upload from user u
-Join(select row_number() over(partition by user_id order by timestamp desc) as row_num,*  from activitylog) as b On u.id= b.user_id and b.row_num=1
-join (SELECT user_id, count(case when type='DELETE' then 1 else NULL end) as TOTAL_DELETE, count(case when type='UPDATE' then 1 else NULL end) as TOTAL_UPDATE,
-count(case when type='INSERT' then 1 else NULL end) as TOTAL_INSERT, case when count(case when timestamp > date_sub(current_date, 2) THEN 1 ELSE NULL END) >= 1 then 'true' else 'false' end as IS_ACTIVE FROM activitylog  group by user_id) as c on u.id=c.user_id Join (select count(*) as total_upload, user_id  from user_upload_dump  group by user_id) as d on d.user_id=u.id ;"
+hive -e "insert into practical_exercise_1.user_report
+Select u.id, al.total_update, al.total_insert, al.total_delete, b.Type as Last_ACTIVITY_Type, al.IS_ACTIVE, d.total_upload from practical_exercise_1.user u 
+left join (select row_number() over(partition by user_id order by timestamp desc) as row_num,*  from practical_exercise_1.activitylog) as b On u.id= b.user_id and b.row_num=1 
+join (select count(*) as total_upload, user_id  from practical_exercise_1.user_upload_dump group by user_id) as d on d.user_id=u.id 
+join (select user_id, count(case when type='INSERT' then 1 else NULL end)as total_insert, count(case when type='UPDATE' then 1 else NULL end) as total_update, count(case when type='DELETE' then 1 else NULL end) as total_delete, (Case when max(from_unixtime(timestamp)) >= date_sub(current_timestamp,2) then 'true' else 'false' end) as IS_ACTIVE
+from practical_exercise_1.activitylog group by user_id) as al On u.id = al.user_id ; "
 
-hive -e "select * from user_report;"
+hive -e "select * from practical_exercise_1.user_report;"
 
 ## inserting data into user_total table   
-hive -e "insert into user_total select CURRENT_TIMESTAMP, count(a.id), count(a.id) from user a;"   
 
-hive -e "insert into user_total select CURRENT_TIMESTAMP, TT , (case when (TT - OLD)=0 then TT else (TT - OLD) end) from ( 
-select row_number() over(order by b.TIME_RAN DESC) as RN,CURRENT_TIMESTAMP,count(a.id) as TT,b.TOTAL_USERS as OLD,MAX(b.TIME_RAN) as MT from user a,user_total b group by b.TIME_RAN,b.TOTAL_USERS order by MT desc ) t1  where RN = 1;" 
+value=$(hive -e "select count(total_users) from practical_exercise_1.user_total;")
+stringarray=($value)
+echo ${stringarray[0]}
+count=${stringarray[0]}
+echo $count
+if [ $count -gt 0 ]; then
+echo "if part"
+hive -e "insert into practical_exercise_1.user_total select CURRENT_TIMESTAMP, TT ,(TT - OLD ) from (select CURRENT_TIMESTAMP, count(user.id) as TT,user_total.TOTAL_USERS as OLD,MAX(user_total.TIME_RAN) as MT from practical_exercise_1.user ,practical_exercise_1.user_total  group by user_total.TIME_RAN,user_total.TOTAL_USERS order by MT desc) t1;"
+else
+echo "else part"
+hive -e "insert into practical_exercise_1.user_total select CURRENT_TIMESTAMP, count(a.id), count(a.id) from practical_exercise_1.user a;"
+fi
 
-hive -e "select * from user_total";
+hive -e "select * from practical_exercise_1.user_total;"
 
